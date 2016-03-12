2e731c82382d3d5e41a84a73c8b0deded044658a
Joao Lopes <jpedrorl@gmail.com>
2016-03-12 15:32:13 -0300

    First commit

---
+++ T1 - Parallel count sort/count_sort_serial.c
@@ -0,0 +1,26 @@
+double count_sort_serial(double a[], int n) {
+	int i, j, count;
+	double *temp;
+	double start, end, duracao;
+
+	temp = (double *)malloc(n*sizeof(double));
+
+	start = omp_get_wtime();
+	for (i = 0; i < n; i++) {
+		count = 0;
+		for (j = 0; j < n; j++)
+			if (a[j] < a[i])
+				count++;
+			else if (a[j] == a[i] && j < i)
+				count++;
+		temp[count] = a[i];
+	}
+	end = omp_get_wtime();
+
+	duracao = end - start;
+
+	memcpy(a, temp, n*sizeof(double));
+	free(temp);
+
+	return duracao;
+}
